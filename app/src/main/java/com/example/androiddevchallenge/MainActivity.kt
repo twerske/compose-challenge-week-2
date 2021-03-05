e094a3dc12386dea42690083d7e24c2fd70b18a6
twerske <pokepsi@gmail.com>
2021-03-04 23:13:51 -0500

    buggy functionality - going to overrite in the morn

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,16 +15,30 @@
  */
 package com.example.androiddevchallenge
 
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
+@ExperimentalFoundationApi
+@ExperimentalAnimationApi
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,13 +51,19 @@ class MainActivity : AppCompatActivity() {
 }
 
 // Start building your app here!
+@ExperimentalFoundationApi
+@ExperimentalAnimationApi
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MyApp() {
+fun MyApp(timeViewModel: TimeViewModel = viewModel()) {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        TimeDisplay(timeViewModel)
     }
 }
 
+@ExperimentalFoundationApi
+@ExperimentalAnimationApi
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
@@ -52,10 +72,97 @@ fun LightPreview() {
     }
 }
 
+@ExperimentalFoundationApi
+@ExperimentalAnimationApi
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
         MyApp()
     }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalFoundationApi
+@OptIn(ExperimentalFoundationApi::class)
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun TimeDisplay(viewModel: TimeViewModel) {
+    val time: Long by viewModel.time.observeAsState(300000L)
+    val countdown = object : CountDownTimer(time, 1000) {
+        override fun onTick(l: Long) {
+            viewModel.onTimeChanged(l)
+        }
+
+        override fun onFinish() {
+
+        }
+    }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        val m = viewModel.getMinutes()
+        val s = viewModel.getSeconds()
+
+        Text(
+            text = "$m : $s",
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = time.toString(),
+            onValueChange = {
+                countdown.cancel()
+                viewModel.onTimeChanged(it.toLong())
+            },
+            label = { Text("Countdown Time") }
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = { countdown.start() }) {
+                Text(text = "START")
+            }
+            Button(onClick = { countdown.start() }) {
+                Text(text = "PAUSE")
+            }
+            Button(onClick = { countdown.cancel() }) {
+                Text(text = "CANCEL")
+            }
+        }
+    }
+}
+
+class TimeViewModel : ViewModel() {
+    private val _time = MutableLiveData(300000L)
+    val time: LiveData<Long> = _time
+
+    fun onTimeChanged(newTime: Long) {
+        _time.value = newTime
+    }
+
+    fun getMinutes(): Long {
+        return (_time.value!! / 1000) / 60
+    }
+
+    fun getSeconds(): Long {
+        return (_time.value!! / 1000) % 60
+    }
+
+//    fun getYears() {
+//        return (_time.value / 1000) / 60
+//    }
+
+//    fun getMonths() {
+//        return (_time.value / 1000) / 60
+//    }
+
+//    fun getDays() {
+//        return (_time.value / 1000) / 60
+//    }
+
+//    fun getHours() {
+//        return (_time.value / 1000) / 60
+//    }
 }
